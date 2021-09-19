a4aaa5978bd9fc22c33f6cbed58ef0cde3ea1a2d
Anatoliy-1986 <Gospodarikov_aa@mail.ru>
2021-09-19 22:24:37 +0300

    fourth commit

--- src/components/About/elements.ts
+++ src/components/About/elements.ts
@@ -73,7 +73,6 @@ export const Information = styled.div`
   display: flex;
   flex-direction: column;
   max-width: 546px;
-  border: 1px solid #202020;
   padding: 45px 0 96px;
   @media ${device.mobile} {
     padding: 0 15px;
@@ -107,71 +106,83 @@ export const Image = styled.img`
 `;
 
 export const Publication = styled.div`
-display: flex;
-max-width: 770px;
-border: 1px solid #202020;
-border-radius: 10px;
-margin-top: 67px;
+  display: flex;
+  max-width: 780px;
+  border: 1px solid #202020;
+  border-radius: 10px;
+  margin-top: 67px;
+  @media ${device.tablet} {
+    flex-wrap: wrap;
+    justify-content: space-around;
+  }
 `;
 
 export const Sidebar = styled.div`
-display: flex;
-flex-direction: column;
-width: 220px;
-border: 1px solid #202020;
+  display: flex;
+  flex-direction: column;
+  width: 220px;
+  border-right: 1px solid black;
+  @media ${device.tablet} {
+    border-left: 1px solid black;
+  }
 `;
 
 export const Expert = styled.div`
-padding: 20px;
-background-color: #cfc;
-
+  padding: 20px;
+  background-color: #cfc;
 `;
-export const Modules = styled.div`
-display: flex;
-border: 1px solid #202020;
 
+export const Modules = styled.div`
+  display: flex;
 `;
 
 export const Block = styled.div`
-flex-grow: 1;
-border: 1px solid #202020;
+  flex-grow: 1;
+  border-top: 1px solid #202020;
+  border-bottom: 1px solid #202020;
+  border-right: 1px solid #202020;
+  &:last-child {
+    border-right: none;
+  }
 `;
 
 export const Heading = styled.div`
-font-size: 22px;
+  font-size: 22px;
   font-weight: bold;
+  padding: 10px 10px 0;
+`;
+
+export const Subtitle = styled.div<{
+  secondary?: boolean;
+  background?: string;
+}>`
+  font-size: 12px;
+  text-transform: ${(props) => (props.secondary ? "uppercase" : "none")};
+  background-color: ${({ background }) => background || "inherit"};
+  opacity: 0.5;
   padding: 10px;
 `;
 
-export const Subtitle = styled.div<{ secondary?: boolean; background?: string}>`
-font-size: 12px;
-text-transform:${(props) => (props.secondary ? "uppercase" : "none")};
-background-color: ${({ background }) => background || "inherit"};
-opacity: 0.5;
-padding: 10px;
-`;
-
 export const List = styled.div`
-display: flex;
-flex-direction: column;
+  display: flex;
+  flex-direction: column;
 `;
 
 export const Parameters = styled.div`
-display: flex;
-flex-direction: column;
-padding: 0 10px;
-border-top: 1px solid black;
-border-bottom: 1px solid black;
-
+  display: flex;
+  flex-direction: column;
+  padding: 0 10px;
+  border-top: 1px solid black;
+  border-bottom: 1px solid black;
 `;
 export const Parameter = styled.div`
-font-size: 17px;
-padding: 10px;
-border-bottom: 1px solid black;
-cursor: pointer;
-&: hover {
+  font-size: 17px;
+  padding: 10px;
+  border-bottom: 1px solid black;
+  cursor: pointer;
+  &: hover {
     background-color: #e2dbfb;
-    transform: scale(1.1)	
+    transform: scale(1.1);
   }
   &:last-child {
     border-bottom: none;
@@ -179,16 +190,28 @@ cursor: pointer;
 `;
 
 export const Article = styled.div`
-border: 1px solid #202020;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  flex-wrap: wrap;
+
+  @media ${device.tablet} {
+    border-left: none;
+  }
 `;
 
 export const Processing = styled.div`
-border: 1px solid #202020;
-margin: 15px;
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  max-width: 600px;
+  border-bottom: 1px solid #202020;
+  padding: 26.5px 15px;
 `;
 
 export const Buttonprocessing = styled.button`
-width: 100px;
+  width: 100px;
   height: 40px;
   font-size: 16px;
   color: black;
@@ -196,13 +219,94 @@ width: 100px;
   border-radius: 5px;
   border: 1px solid black;
   text-decoration: none;
-  margin: 10px;
+  margin: 10px 0;
   cursor: pointer;
   &: hover {
     box-shadow: inset 2px 2px 5px #000000, 1px 1px 5px #000000;
     color: white;
     background-color: black;
-    transform: scale(1.1)	
+    transform: scale(1.1);
   }
 `;
 
+export const Recording = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 10px 0;
+  @media ${device.tablet} {
+    flex-wrap: wrap;
+    justify-content: space-around;
+  }
+`;
+
+export const Range = styled.input`
+-webkit-appearance: none;
+width: 220px;
+  height: 2px;
+  background: #D3D3D3;
+outline: none;
+margin: 0 10px;
+cursor: pointer;
+&::-webkit-slider-thumb {
+    .thumb();
+ -webkit-appearance: none;
+ background:black;
+ width: 10px;
+  height: 10px;
+  border-radius: 50%;
+  cursor: pointer;
+  }
+`;
+
+export const Label = styled.label`
+  padding: 5px 20px;
+  border: 1px solid #202020;
+  border-radius: 10em/10em;
+  margin: 0 15px;
+`;
+
+export const Raise = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 5px 10px;
+  border: 1px solid #202020;
+  border-radius: 5px;
+  margin: 0;
+  @media ${device.tablet} {
+    margin: 15px 0;
+  }
+`;
+
+export const Percent = styled.span`
+  font-size: 18px;
+  margin-right: 20px;
+`;
+
+export const Percentbutton = styled.button`
+  border: 1px solid #202020;
+  border-radius: 50px;
+
+  background-color: transparent;
+  margin: 5px;
+  border: 1px solid black;
+  text-decoration: none;
+
+  cursor: pointer;
+`;
+
+export const Theory = styled.div`
+  border: 1px solid #202020;
+  max-width: 450px;
+  max-height: 335px;
+  padding: 30px;
+  border-radius: 20px 20px 0 0;
+  border-bottom: none;
+  overflow: hidden;
+  overflow-y: scroll;
+  &::-webkit-scrollbar {
+    width: 0px;
+    background: transparent;
+  }
+`;
--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 import upword from "./images/upword.png";
 import { Container, ButtonGeneral } from "../shared";
 import {
@@ -23,6 +24,13 @@ import {
   Parameter,
   Processing,
   Buttonprocessing,
+  Recording,
+  Range,
+  Label,
+  Raise,
+  Percent,
+  Percentbutton,
+  Theory,
 } from "./elements";
 
 const blocks = [
@@ -70,7 +78,26 @@ const buttons = [
   },
 ];
 
+const rangeSlider = {
+  max: 100,
+  min: 0,
+  val: 0,
+};
+
 function About() {
+  const [slider, setSlider] = useState(rangeSlider);
+
+  function onSlide(prop: string, event: any) {
+    setSlider({ ...slider, ...{ [prop]: event.target.value } });
+    const removeButton: any = document.querySelector(Range);
+    removeButton.style.background =
+      "-webkit-linear-gradient(left, black 0%, black " +
+      event.target.value +
+      "%, #D3D3D3 " +
+      event.target.value +
+      "%, #D3D3D3 100%)";
+  }
+
   return (
     <Container>
       <Header>
@@ -116,14 +143,62 @@ function About() {
             ))}
           </Sidebar>
           <Article>
-          <Processing>
-          {buttons.map((item, id) => (
-                <Buttonprocessing key={item.id}>
-                  {item.text}
-                </Buttonprocessing>
+            <Processing>
+              {buttons.map((item, id) => (
+                <Buttonprocessing key={item.id}>{item.text}</Buttonprocessing>
               ))}
-</Processing>
-            <input type="range" min="8" max="48" step="1" value="14" />
+              <Recording>
+                {slider.val}
+                <Range
+                  type="range"
+                  min={slider.min}
+                  max={slider.max}
+                  id="time"
+                  step="1"
+                  value={slider.val}
+                  onChange={(event) => onSlide("val", event)}
+                />
+                {slider.max}
+                <Label htmlFor="time">&#215; 1</Label>
+                <Raise>
+                  <Percent>175%</Percent>
+                  <Percentbutton>-</Percentbutton>
+                  <Percentbutton>+</Percentbutton>
+                </Raise>
+              </Recording>
+            </Processing>
+            <Theory>
+              "At vero eos et accusamus et iusto odio dignissimos ducimus qui
+              blanditiis praesentium voluptatum deleniti atque corrupti quos
+              dolores et quas molestias excepturi sint occaecati cupiditate non
+              provident, similique sunt in culpa qui officia deserunt mollitia
+              animi, id est laborum et dolorum fuga. Et harum quidem rerum
+              facilis est et expedita distinctio. Nam libero tempore, cum soluta
+              nobis est eligendi optio cumque nihil impedit quo minus id quod
+              maxime placeat facere possimus, omnis voluptas assumenda est,
+              omnis dolor repellendus. Temporibus autem quibusdam et aut
+              officiis debitis aut rerum necessitatibus saepe eveniet ut et
+              voluptates repudiandae sint et molestiae non recusandae. Itaque
+              earum rerum hic tenetur a sapiente delectus, ut aut reiciendis
+              voluptatibus maiores alias consequatur aut perferendis doloribus
+              asperiores repellat." 1914 translation by H. Rackham "On the other
+              hand, we denounce with righteous indignation and dislike men who
+              are so beguiled and demoralized by the charms of pleasure of the
+              moment, so blinded by desire, that they cannot foresee the pain
+              and trouble that are bound to ensue; and equal blame belongs to
+              those who fail in their duty through weakness of will, which is
+              the same as saying through shrinking from toil and pain. These
+              cases are perfectly simple and easy to distinguish. In a free
+              hour, when our power of choice is untrammelled and when nothing
+              prevents our being able to do what we like best, every pleasure is
+              to be welcomed and every pain avoided. But in certain
+              circumstances and owing to the claims of duty or the obligations
+              of business it will frequently occur that pleasures have to be
+              repudiated and annoyances accepted. The wise man therefore always
+              holds in these matters to this principle of selection: he rejects
+              pleasures to secure other greater pleasures, or else he endures
+              pains to avoid worse pains."
+            </Theory>
           </Article>
         </Publication>
       </Concerning>
